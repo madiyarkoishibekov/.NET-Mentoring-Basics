ccefaf8f18873a5d51a66619e1aedbcc9d781f3a
madiyarkoishibekov <madiyarkoishibekov@gmail.com>
2022-02-27 22:43:51 +0600

    Module 1. Task 1,2

---
+++ Module 1/Task 1/dotNET-Mentoring-Basics/ClassLibrary/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class UserInfo
+    {
+        public string UserName { get; set; }
+        public string UserGreetingMessage()
+        {
+            if (string.IsNullOrEmpty(UserName))
+            { return $"{default(DateTime)}\nGreetings, Default User!"; }
+            return $"{default(DateTime)}\nGreetings, {UserName}!";
+        }
+    }
+}
---
+++ Module 1/Task 1/dotNET-Mentoring-Basics/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using System;
+namespace ConsoleApp
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please enter your name:");
+            UserInfo user = new UserInfo()
+            {
+                UserName = Console.ReadLine()
+            };
+
+            Console.WriteLine(user.UserGreetingMessage());
+        }
+    }
+}
