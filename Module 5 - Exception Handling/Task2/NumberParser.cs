dd18c489154b1f2bef908652e1b7adbd83ff3904
Madiyar Koishibekov <madiyar.koishibekov@gmail.com>
2022-03-27 13:07:44 +0600

    Module 4 Tasks

---
+++ Module 5 - Exception Handling/Task2/NumberParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public int Parse(string stringValue)
+        {
+            if (stringValue == null)
+            {
+                throw new ArgumentNullException(stringValue);
+            }
+
+            int intValue;
+
+            try
+            {
+                intValue = Convert.ToInt32(stringValue);
+                return intValue;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ Module 5 - Exception Handling/Task3/UserTaskController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Task3.DoNotChange;
+
+namespace Task3
+{
+    public class UserTaskController
+    {
+        private readonly UserTaskService _taskService;
+
+        public UserTaskController(UserTaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        public bool AddTaskForUser(int userId, string description, IResponseModel model)
+        {
+            string message = GetMessageForModel(userId, description);
+            if (message != null)
+            {
+                model.AddAttribute("action_result", message);
+                return false;
+            }
+
+            return true;
+        }
+
+        private string GetMessageForModel(int userId, string description)
+        {
+            var task = new UserTask(description);
+            int result = _taskService.AddTaskForUser(userId, task);
+            if (result == -1)
+            {
+                return "Invalid userId";
+                throw new ArgumentOutOfRangeException(nameof(result));
+            }
+
+            if (result == -2)
+            {
+                return "User not found";
+                throw new ArgumentNullException(nameof(result));
+            }
+
+            if (result == -3)
+            {
+                return "The task already exists";
+                throw new Exception(nameof(result));
+            }
+
+            return null;
+        }
+    }
+}
