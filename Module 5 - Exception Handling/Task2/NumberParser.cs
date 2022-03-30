ee6ee05c355463bca42ac1fc3cc8eabb0eb86ad5
Madiyar Koishibekov <madiyar.koishibekov@gmail.com>
2022-03-30 11:43:30 +0600

    Implement own algorithm for parsing from string to int

--- Module 5 - Exception Handling/Task2/NumberParser.cs
+++ Module 5 - Exception Handling/Task2/NumberParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task2
 {
@@ -8,21 +9,39 @@ namespace Task2
         {
             if (stringValue == null)
             {
-                throw new ArgumentNullException(stringValue);
+                throw new ArgumentNullException("No string provided.");
             }
 
-            int intValue;
+            stringValue = stringValue.Trim();
+            var digits = new List<char>();
+            var isNegativeNumber = false;
+            digits.AddRange(stringValue.ToCharArray());
+            if (digits.Count > 0 && (digits[0] == '-' || digits[0] == '+'))
+            {
+                isNegativeNumber = digits[0] == '-';
+                digits.RemoveAt(0);
+            }
 
-            try
+            if (!digits.TrueForAll(c => (c >= '0') && (c <= '9')) || digits.Count == 0)
             {
-                intValue = Convert.ToInt32(stringValue);
-                return intValue;
+                throw new FormatException("Not a number.");
             }
-            catch (FormatException ex)
+
+            var result = GetIntFromListOfDigits(digits, isNegativeNumber);
+            return result;
+        }
+
+        private int GetIntFromListOfDigits(List<char> digits, bool isNegativeNumber)
+        {
+            var multiplier = isNegativeNumber ? -1 : 1;
+            int result = 0;
+            for (int i = digits.Count - 1; i >= 0; i--)
             {
-                Console.WriteLine(ex.Message);
-                throw;
+                result = checked(result + (digits[i] - '0') * multiplier);
+                multiplier *= 10;
             }
+
+            return result;
         }
     }
 }
